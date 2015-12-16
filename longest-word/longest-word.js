4df9fb7c5af324e05ab1121dfbddd42d1841b20f
Davidslc <djjohnson@gmail.com>
2015-12-15 19:14:57 -0700

    Created a longest-word counter.

---
+++ longest-word/longest-word.js
@@ -0,0 +1,19 @@
+function findLongestWord(str) {
+  var word = str.split(' '); // Create an array of the words.
+  var longest = 0; // Start the word comparison at zero.
+  
+  for (var i = 0; i < word.length; i++) {
+    var removeNonAlpha = word[i].replace(/[_\W]/g, ''); // Remove non-alpha characters that may to connected to a word.
+    if (removeNonAlpha.length > longest) { // Check to see if the current word is bigger than the last.
+      longest = removeNonAlpha.length; // If it is, replace it with the current length.
+    }
+  }
+  return "The length of the longest word is " + longest + " characters."; // At the end of the 'for' loop, the 'longest' variable will have the length of the longest word.
+}
+
+function getOption() {
+    var obj = document.getElementById("mySelect").value;
+    var word = findLongestWord(obj);
+    document.getElementById("demo").innerHTML = word;
+}
+
